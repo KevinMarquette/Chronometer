b7c118945d8bcf688e13ac7666a3e9cb8b061051
KevinMarquette <kevmar@gmail.com>
2017-08-05 19:53:22 -0700

    resolve merge conflict

--- Chronometer/Classes/Chronometer.ps1
+++ Chronometer/Classes/Chronometer.ps1
@@ -25,11 +25,7 @@ class Chronometer
 
                 $breakpointParam = @{
                     Script = $file
-<<<<<<< HEAD
                     Line   = $bpLine
-=======
-                    Line = $bpLine
->>>>>>> master
                     Action = { [ScriptProfiler]::RecordExecution( $_) }
                 }
                 $this.breakPoint += Set-PSBreakpoint @breakpointParam
