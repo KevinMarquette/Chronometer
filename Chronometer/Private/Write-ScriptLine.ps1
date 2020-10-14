2950a65f9cb4589ef79c2c9e11b12c9002fd0f94
Maarten <maarten.piederiet@gmail.com>
2020-10-14 12:10:22 +0200

    Added formatting options (HTML, GridView)

--- Chronometer/Private/Write-ScriptLine.ps1
+++ Chronometer/Private/Write-ScriptLine.ps1
@@ -3,28 +3,72 @@ function Write-ScriptLine
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
     [cmdletbinding()]
     param(
-        [scriptline]
-        $line,
+        [scriptline]$line,
         $WarningAt = [int]::MaxValue,
-        $ErrorAt = [int]::MaxValue
+        $ErrorAt = [int]::MaxValue,
+        [Parameter(Mandatory=$True)][ValidateSet('FormattedText','HTML','GridView')]
+        [string]$OutputMethod
     )
 
     if ( $line )
-    {         
-        $Color = 'Green'
-        if ( $line.HitCount -eq 0 )
-        {
-            $Color = 'Gray'
+    {
+        switch ($OutputMethod) {
+            'FormattedText' {
+                $Color = 'Green'
+                if ( $line.HitCount -eq 0 )
+                {
+                    $Color = 'Gray'
+                }
+                elseif ( $line.Average -ge $ErrorAt )
+                {
+                    $Color = 'Red'
+                }
+                elseif ( $line.Average -ge $WarningAt )
+                {
+                    $Color = 'Yellow'
+                }
+                Write-Host $line.toString() -ForegroundColor $Color
+                break
+            }
+            'HTML' {
+                $Class = 'OK'
+                if ( $line.HitCount -eq 0 )
+                {
+                    $Class = 'Unused'
+                }
+                elseif ( $line.Average -ge $ErrorAt )
+                {
+                    $Class = 'Error'
+                }
+                elseif ( $line.Average -ge $WarningAt )
+                {
+                    $Class = 'Warning'
+                }
+                Write-Output ('      <tr class="{0}">{1}</tr>' -f $Class,($line.toString($True)))
+                break
+            }
+            'GridView' {
+                $Line | Select-Object -Property @{
+                    Name = 'Total ms'
+                    Expression = { [Math]::Round($_.Duration.TotalMilliseconds, 0) }
+                }, @{
+                    Name = 'HitCount'
+                    Expression = { [Math]::Round($_.HitCount, 0) }
+                }, @{
+                    Name = 'Average ms'
+                    Expression = {[Math]::Round($_.Average, 1)}
+                }, @{
+                    Name = 'Line #'
+                    Expression = { $_.LineNumber }
+                }, @{
+                    Name = 'Line'
+                    Expression = { $_.Text }
+                }
+                break
+            }
+            default {
+                Throw [System.NotImplementedException]
+            }
         }
-        elseif ( $line.Average -ge $ErrorAt )
-        {
-            $Color = 'Red'
-        }
-        elseif ( $line.Average -ge $WarningAt )
-        {
-            $Color = 'Yellow'
-        }
-        
-        Write-Host $line.toString() -ForegroundColor $Color
     }
 }
--- Chronometer/Public/Format-Chronometer.ps1
+++ Chronometer/Public/Format-Chronometer.ps1
@@ -4,7 +4,7 @@ function Format-Chronometer
         .Description
         Generates a report from a Chronometer
 
-        .Example                
+        .Example
         $script = ls C:\workspace\PSGraph\PSGraph -Recurse -Filter *.ps1
         $resultes = Get-Chronometer -Path $script.fullname  -ScriptBlock {Invoke-Pester C:\workspace\PSGraph}
         $results | Format-Chronometer -WarnAt 20 -ErrorAt 200
@@ -38,33 +38,186 @@ function Format-Chronometer
 
         # Forces the report to show scripts with no execution time
         [switch]
-        $ShowAll
+        $ShowAll,
+
+        # Define the output method
+        [Parameter(Mandatory=$false)][ValidateSet('FormattedText','HTML','GridView')]
+        [string]$OutputMethod='FormattedText'
+
     )
+    begin {
+        switch ($OutputMethod) {
+            'GridView' {
+                $GridViewOutput = New-Object System.Collections.Arraylist
+                break
+            }
+            'HTML' {
+                # Only write the HTML file header the first time this function is called
+                if ($PsCmdlet.ParameterSetName -eq 'Script') {
+                    $HeaderWritten=$False
+                    # Write HTML file header
+                    $HTMLHeader=@'
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.21/fh-3.1.7/datatables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.21/fh-3.1.7/datatables.min.js"></script>
+<script type="text/javascript" class="init">
+$(document).ready(function() {
+$('table.chronometer').DataTable({
+    "order": [[ 3, "asc" ]],
+    "dom": '<"top"fi>rt<"bottom"lp>',
+    "paging": false, // false to disable pagination (or any other option)
+    "fixedHeader": true,
+    "aoColumns": [
+        { "orderSequence": [ "desc", "asc" ] },
+        { "orderSequence": [ "desc", "asc" ] },
+        { "orderSequence": [ "desc", "asc" ] },
+        null,
+        null
+    ],
+    "language": {
+        "search": "_INPUT_",            // Removes the 'Search' field label
+        "searchPlaceholder": "Search"   // Placeholder for the search box
+    }
+});
+} );
+</script>
+<style>
+caption {
+caption-side: top
+}
+.dataTables_filter {
+float: left !important;
+text-align: left;
+margin-top: 10px;
+left: 0px;
+top: 0px;
+}
+.dataTables_info {
+float: right !important;
+position: relative;
+right: 0px;
+top: -30px;
+}
+table, th, td {
+#  border: 1px solid white;
+#  background-color: #000000;
+border-collapse: collapse;
+}
+th, td {
+padding: 0px;
+text-align: left;
+}
+.OK {
+color: green;
+}
+.warning {
+color: orange;
+}
+.error {
+color: red;
+}
+.unused {
+color: gray;
+}
+
+</style>
+</head>
+<body>
+'@
+                    break
+                }
+            }
+        }
+    }
 
     process
     {
         try
         {
             foreach ( $script in $InputObject )
-            {            
+            {
                 if ( $script.ExecutionTime -ne [TimeSpan]::Zero -or $ShowAll )
                 {
-                    Write-Host ''
-                    Write-Host "Script: $($script.Path)" -ForegroundColor Green
-                    Write-Host "Execution Time: $($script.ExecutionTime)" -ForegroundColor Green
-
-                    $script.line | Format-Chronometer -WarningAt $WarningAt -ErrorAt $ErrorAt
+                    switch ($OutputMethod) {
+                        'HTML'  {
+                            if (-not $HeaderWritten) {
+                                Write-Output $HTMLHeader
+                                $HeaderWritten=$True
+                            }
+                            # Table element for each script
+                            Write-Output @'
+    <table id="" class="chronometer table row-border compact order-column hover">
+    <caption>
+'@
+                            # Caption text
+                            Write-Output @"
+    Script: $($script.Path)<br>
+    Execution Time: $($script.ExecutionTime)<br>
+"@
+                            # Table header
+                            Write-Output @'
+    </caption>
+    <thead>
+        <tr><th>Total ms</th><th>Hit cnt</th><th>Avg ms</th><th>Line#</th><th>Line</th></tr>
+    </thead>
+    <tbody>
+'@
+                            break
+                        }
+                        'FormattedText' {
+                            Write-Host ''
+                            Write-Host "Script: $($script.Path)" -ForegroundColor Cyan
+                            Write-Host "Execution Time: $($script.ExecutionTime)" -ForegroundColor Cyan
+                            break
+                        }
+                        'GridView' {
+                            $GridViewTitle = "Chronometer Results: $($script.Path)"
+                            break
+                        }
+                    }
+                    $script.line | Format-Chronometer -WarningAt $WarningAt -ErrorAt $ErrorAt -OutputMethod $OutputMethod
+                    switch ($OutputMethod) {
+                        'HTML' {
+                            Write-Output @'
+    </tbody>
+    </table>
+'@
+                            break
+                        }
+                    }
                 }
             }
 
-            foreach ( $command in $Line )
-            {
-                Write-ScriptLine $command -WarningAt $WarningAt -ErrorAt $ErrorAt
-            }            
+            foreach ( $command in $Line ) {
+                if ($OutputMethod -eq 'GridView') {
+                    $null=$GridViewOutput.Add((Write-ScriptLine $command -WarningAt $WarningAt -ErrorAt $ErrorAt -OutputMethod $OutputMethod))
+                } Else {
+                    Write-ScriptLine $command -WarningAt $WarningAt -ErrorAt $ErrorAt -OutputMethod $OutputMethod
+                }
+            }
         }
-        catch
-        {
+        catch {
             $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+    end {
+        switch ($OutputMethod) {
+            'HTML' {
+                # Make sure to also write the HTML closing tags if the HTML header was written
+                if ($PsCmdlet.ParameterSetName -eq 'Script' -and $HeaderWritten) {
+                    # Write HTML closing tag
+                    Write-Output @'
+</body>
+</html>
+'@
+                }
+            }
+            'GridView' {
+                if ($GridViewOutput.Count -gt 0) {
+                    $GridViewOutput | Out-GridView -Title $GridViewTitle
+                }
+            }
         }
     }
 }
